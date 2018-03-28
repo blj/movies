682d2dc0d6d409c20f45c9b6c550d7df6288f6b8
Balaji Raghavan <b.raghavan@gmail.com>
2018-03-28 09:15:06 -0400

    rubocop recommendations

--- spec/features/features_spec.rb
+++ spec/features/features_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 describe 'Features' do
   it 'has a title' do
--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 describe 'Homepage' do
   it 'has a title' do
     visit '/'
-    expect(page).to have_title("Movies")
+    expect(page).to have_title('Movies')
   end
 end
