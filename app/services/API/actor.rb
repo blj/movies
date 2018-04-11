35b46546904bad7112a127c8cc05ef693a49f5b6
Balaji Raghavan <b.raghavan@gmail.com>
2018-04-11 12:32:22 -0500

    API for actor

---
+++ app/services/API/actor.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+# Actor index resource http://34.216.164.119/actors
+# returns ids in JSON array
+# [2,3,4,5,6,7,8,9,11,12,13,14]
+
+# Actor single resource http://34.216.164.119/actors/2
+# returns item attribute {"id":2,"name":"Simon Pegg","movies":[1,2,3,4,6]}
+# non existent resource id returns 404 status code
+
+class API::Actor
+  # site = 'http://34.216.164.119/'  
+  def self.ids
+    API::Connection.get('/actors')
+  end
+  def self.get(id)
+    API::Connection.get("/actors/#{id}")
+  end
+end
---
+++ spec/services/api/actor_spec.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe 'Actor' do
+  let(:actor_ids) {
+    VCR.use_cassette('actors') do
+      API::Actor.ids
+    end
+  }
+  let(:a_actor) {
+    VCR.use_cassette 'actors[0]' do
+      a_actor = API::Actor.get(actor_ids[0])
+    end
+  }
+  it 'loads id of all movies in an array' do
+    expect(actor_ids).kind_of? Array
+    expect(actor_ids[0]).kind_of? Integer
+  end
+  it 'loads an individual actor for an id' do
+    expect(a_actor).to include("id", "name", "movies")
+  end
+end
