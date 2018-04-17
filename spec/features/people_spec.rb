e724fb4746f289331999dceaa8144fe01132bc6c
Balaji Raghavan <b.raghavan@gmail.com>
2018-04-16 19:49:18 -0500

    people should be listed along with the movies they acted and directed

--- spec/features/people_spec.rb
+++ spec/features/people_spec.rb
@@ -11,10 +11,18 @@ describe 'People' do
     it 'has a title' do
       expect(page).to have_title('Actors and Directors')
     end
-    it 'lists actor names' do
-      within('#people') do
-        expect(page).to have_text('Simon Pegg')
-        expect(page).to have_text('Nick Frost')
+    it 'lists people with acted and directed names' do
+      within('#person_2') do
+        expect(page).to have_selector('.name', text: 'Simon Pegg')
+        expect(page).to have_selector('.directed', text: "Simon's Cam")
+        expect(page).to have_selector('.acted', text: "Hot Fuzz, Simon's Cam, Shaun of the Dead, The World's End, and Star Trek Beyond")
+      end
+    end
+    it 'lists people with acted and directed names' do
+      within('#person_3') do
+        expect(page).to have_selector('.name', text: 'Nick Frost')
+        expect(page).to have_selector('.directed', text: '')
+        expect(page).to have_selector('.acted', text: "Hot Fuzz, Simon's Cam, Shaun of the Dead, and The World's End")
       end
     end
   end
